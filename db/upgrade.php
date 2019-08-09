64b36efe91059e5f301df06f4e3ba69c5af00ae2
Willian Mano <willianmanoaraujo@gmail.com>
2019-08-09 09:52:33 -0300

    first commit

---
+++ db/upgrade.php
@@ -0,0 +1,87 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Book module upgrade code
+ *
+ * @package    mod_book
+ * @copyright  2009-2011 Petr Skoda {@link http://skodak.org}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * Book module upgrade task
+ *
+ * @param int $oldversion the version we are upgrading from
+ * @return bool always true
+ */
+function xmldb_book_upgrade($oldversion) {
+    global $DB;
+
+    // Automatically generated Moodle v3.3.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    // Automatically generated Moodle v3.4.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    // Automatically generated Moodle v3.5.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    // Automatically generated Moodle v3.6.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    // Automatically generated Moodle v3.7.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    $dbman = $DB->get_manager();
+
+    if ($oldversion < 2019062000) {
+
+        // Adds the new field to the user completion criteria.
+        $table = new xmldb_table('book');
+        $field = new xmldb_field('completionview', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, 0, 'revision');
+
+        // Conditionally launch add field.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Define table book_chapters_userviews to be created.
+        $table = new xmldb_table('book_chapters_userviews');
+
+        // Adding fields to table book_chapters_userviews.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('chapterid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+
+        // Adding keys to table book_chapters_userviews.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+        $table->add_key('chapterid', XMLDB_KEY_FOREIGN, array('chapterid'), 'book_chapters', array('id'));
+        $table->add_key('userid', XMLDB_KEY_FOREIGN, array('userid'), 'user', array('id'));
+
+        // Conditionally launch create table for book_chapters_userviews.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Book savepoint reached.
+        upgrade_mod_savepoint(true, 2019062000, 'book');
+    }
+
+    return true;
+}
---
+++ mod_form.php
@@ -0,0 +1,163 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Instance add/edit form
+ *
+ * @package    mod_book
+ * @copyright  2004-2011 Petr Skoda {@link http://skodak.org}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+require_once(__DIR__.'/locallib.php');
+require_once($CFG->dirroot.'/course/moodleform_mod.php');
+
+class mod_book_mod_form extends moodleform_mod {
+
+    function definition() {
+        global $CFG;
+
+        $mform = $this->_form;
+
+        $config = get_config('book');
+
+        $mform->addElement('header', 'general', get_string('general', 'form'));
+
+        $mform->addElement('text', 'name', get_string('name'), array('size'=>'64'));
+        if (!empty($CFG->formatstringstriptags)) {
+            $mform->setType('name', PARAM_TEXT);
+        } else {
+            $mform->setType('name', PARAM_CLEANHTML);
+        }
+        $mform->addRule('name', null, 'required', null, 'client');
+        $mform->addRule('name', get_string('maximumchars', '', 255), 'maxlength', 255, 'client');
+        $this->standard_intro_elements(get_string('moduleintro'));
+
+        // Appearance.
+        $mform->addElement('header', 'appearancehdr', get_string('appearance'));
+
+        $alloptions = book_get_numbering_types();
+        $allowed = explode(',', $config->numberingoptions);
+        $options = array();
+        foreach ($allowed as $type) {
+            if (isset($alloptions[$type])) {
+                $options[$type] = $alloptions[$type];
+            }
+        }
+        if ($this->current->instance) {
+            if (!isset($options[$this->current->numbering])) {
+                if (isset($alloptions[$this->current->numbering])) {
+                    $options[$this->current->numbering] = $alloptions[$this->current->numbering];
+                }
+            }
+        }
+        $mform->addElement('select', 'numbering', get_string('numbering', 'book'), $options);
+        $mform->addHelpButton('numbering', 'numbering', 'mod_book');
+        $mform->setDefault('numbering', $config->numbering);
+
+        $alloptions = book_get_nav_types();
+        $allowed = explode(',', $config->navoptions);
+        $options = array();
+        foreach ($allowed as $type) {
+            if (isset($alloptions[$type])) {
+                $options[$type] = $alloptions[$type];
+            }
+        }
+        if ($this->current->instance) {
+            if (!isset($options[$this->current->navstyle])) {
+                if (isset($alloptions[$this->current->navstyle])) {
+                    $options[$this->current->navstyle] = $alloptions[$this->current->navstyle];
+                }
+            }
+        }
+        $mform->addElement('select', 'navstyle', get_string('navstyle', 'book'), $options);
+        $mform->addHelpButton('navstyle', 'navstyle', 'mod_book');
+        $mform->setDefault('navstyle', $config->navstyle);
+
+        $mform->addElement('checkbox', 'customtitles', get_string('customtitles', 'book'));
+        $mform->addHelpButton('customtitles', 'customtitles', 'mod_book');
+        $mform->setDefault('customtitles', 0);
+
+        $this->standard_coursemodule_elements();
+
+        $this->add_action_buttons();
+    }
+
+    /**
+     * Process the data before load the form
+     *
+     * @param array $defaultvalues
+     */
+    public function data_preprocessing(&$defaultvalues) {
+        if($defaultvalues['completionview'] != "0") {
+            $defaultvalues['completionviewactive'] = 1;
+        }
+    }
+
+    /**
+     * Process data after form submit
+     * @param stdClass $data
+     * @return stdClass|void
+     */
+    public function data_postprocessing($data) {
+        if(empty($data->completionviewactive)) {
+            $data->completionview = "0";
+        }
+
+        unset($data->completionviewactive);
+
+        return $data;
+    }
+
+    /**
+     * Book completion rule fields.
+     *
+     * @return array|void
+     *
+     * @throws coding_exception
+     */
+    public function add_completion_rules() {
+        $mform = $this->_form;
+
+        $completionviews = [];
+        for ($i = 10; $i <= 100; $i+=10) {
+            $completionviews[$i] = $i . '%';
+        }
+
+        $group = [
+            $mform->createElement('checkbox', 'completionviewactive', '   ', get_string('requiredcompletionview', 'book')),
+            $mform->createElement('select', 'completionview', get_string('completionviewselect', 'book'), $completionviews),
+        ];
+
+        $mform->addGroup($group, 'completionviewgroup', get_string('completionviewselect', 'book'), ['<br>'], false);
+        $mform->disabledIf('completionview', 'completionviewactive', 'notchecked');
+
+        return ['completionviewgroup'];
+    }
+
+    /**
+     * Called during validation to see whether some module-specific completion rules are selected.
+     *
+     * @param array $data Input data not yet validated.
+     *
+     * @return bool True if one or more rules is enabled, false if none are.
+     */
+    public function completion_rule_enabled($data) {
+        return (!empty($data['completionviewactive']) && $data['completionview'] > 0);
+    }
+}
